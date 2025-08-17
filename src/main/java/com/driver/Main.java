c0da2bf6193adeb98260ec763f8996e071c4f19f
prakash <prakasharyansingh05@gmail.com>
2025-08-17 20:42:05 +0530

    assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
+        Boat b = new Boat("Titanic",3000);
+        System.out.println("Boat is " + b.getVehicleName() + " " + "Capacity of boat is  " + b.getVehicleCapacity());     // Titanic
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
     }
 }
