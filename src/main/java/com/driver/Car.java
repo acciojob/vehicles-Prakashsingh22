1c59f11d15f83b5ed5cbde4947850d70879ffd4c
prakash <prakasharyansingh05@gmail.com>
2025-08-17 21:49:23 +0530

    assignment completed

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -20,14 +20,13 @@ public class Car extends Vehicle {
         this.seats = seats;
         this.currentGear = 1;
     }
-
-    public void changeGear(int newGear){
+    public void changeGear(int newGear) {
         this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        move(newSpeed, newDirection);
+        super.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
     public int getCurrentGear(){
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -11,29 +11,10 @@ public class F1 extends Car {
     }
 
     public void accelerate(int rate){
+        int newSpeed = 0;
+        //set the value of new speed by using currentSpeed and rate
+        newSpeed = getCurrentSpeed() + rate;
 
-        int newSpeed = getCurrentSpeed() + rate;
-
-        if(newSpeed == 0) {
-            stop();
-            changeGear(1);
-        } else if(newSpeed > 0 && newSpeed <= 50) {
-            changeGear(1);
-        } else if(newSpeed > 50 && newSpeed <= 100) {
-            changeGear(2);
-        } else if(newSpeed > 100 && newSpeed <= 150) {
-            changeGear(3);
-        } else if(newSpeed > 150 && newSpeed <= 200) {
-            changeGear(4);
-        } else if(newSpeed > 200 && newSpeed <= 250) {
-            changeGear(5);
-        } else {
-            changeGear(6);
-        }
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }//set the value of new speed by using currentSpeed and rate
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -43,8 +24,27 @@ public class F1 extends Car {
          * speed 201-250: gear
          * speed more than 250: gear 6
          */
-
-
-
+        if(newSpeed == 0) {
+            stop();
+            changeGear(1);
+            //Stop the car, set gear as 1
+        }
+        //for all other cases, change the gear accordingly
+        else if(newSpeed > 0 && newSpeed <= 50) {
+            changeGear(1);
+        } else if(newSpeed > 50 && newSpeed <= 100) {
+            changeGear(2);
+        } else if(newSpeed > 100 && newSpeed <= 150) {
+            changeGear(3);
+        } else if(newSpeed > 150 && newSpeed <= 200) {
+            changeGear(4);
+        } else if(newSpeed > 200 && newSpeed <= 250) {
+            changeGear(5);
+        } else if(newSpeed > 250) {
+            changeGear(6);
+        }
+        if(newSpeed > 0) {
+            changeSpeed(newSpeed, getCurrentDirection());
+        }
     }
 }
